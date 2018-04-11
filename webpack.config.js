9816418d75263880e1ff74f0f68fd6e0c94ad6e3
Or Dabool <o.dabool@duplitrade.com>
2018-04-11 09:45:12 +0300

    Add babel loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,16 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: [ 'babel-loader' ]
-      // },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      },
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
