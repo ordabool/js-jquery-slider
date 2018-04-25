822837115c22cb73fd332bd8d451b22b5137ac18
Or Dabool <o.dabool@duplitrade.com>
2018-04-25 18:24:46 +0300

    Add single slider support

--- src/js/dupli-slider.js
+++ src/js/dupli-slider.js
@@ -25,27 +25,25 @@ class SliderManager {
           } else {
             if (entryValue != null) {
               var entryIndex = values.indexOf(entryValue);
-              dupliSlider.setAttribute("value",entryIndex);
+              dupliSlider.value = entryIndex;
+              dupliSlider.setAttribute("current-value",values[entryIndex]);
+              dupliSlider.setAttribute("current-index",entryIndex);
             } else {
-              dupliSlider.setAttribute("value",0);
+              dupliSlider.value = 0;
+              dupliSlider.setAttribute("current-value",values[0]);
+              dupliSlider.setAttribute("current-index",0);
             }
           }
           length = values.length - 1;
           dupliSlider.setAttribute("max",length);
           dupliSlider.setAttribute("min",0);
           dupliSlider.setAttribute("step",1);
-          dupliSlider.setAttribute("current-value",values[0]);
-          dupliSlider.setAttribute("current-index",0);
           outputs[i].innerHTML = "<strong>" + values[0] + "</strong>";
         }
-        
 
       var dupliSlider, newPoint, newPlace, offset;
       var setupSlider = function($slider){
-        console.log("dupliSlider: " + dupliSlider);
-        console.log("slider: " + slider);
-        console.log("$slider: " + $slider);
-        width = dupliSlider.width();
+        var width = dupliSlider.width();
         var valuesString = dupliSlider.attr("slider-values");
         var values = valuesString.split('~');
         length = values.length - 1;
@@ -61,18 +59,17 @@ class SliderManager {
           } else { 
             newPlace = width * newPoint + offset; offset -= newPoint;
           }
-      
+
           var sliderHighlight = dupliSlider.prev("span");
           sliderHighlight.width(newPlace+5.8);
-           
+
           var sliderOutput = dupliSlider.next("output");
-      
+
           sliderOutput.html('<strong>'+values[dupliSlider.val()]+'</strong>');
-            // .html('<strong>$'+values[dupliSlider.val()]+'</strong>&nbsp;/&nbsp;wk');
-      
+
           dupliSlider.attr("current-value",values[dupliSlider.val()]);
           dupliSlider.attr("current-index",dupliSlider.val());
-            
+
           if(newPoint <= .08){
             sliderOutput
               .css({
@@ -96,7 +93,6 @@ class SliderManager {
               })
             }
         }
-        
       }
 
       // Select all range inputs, watch for change
@@ -105,15 +101,130 @@ class SliderManager {
         setupSlider(dupliSlider);
       })
       .trigger('change');	
-      
       $(".dupli-slider").on('input', function() {
         dupliSlider = $(this);
         setupSlider(dupliSlider);
       })
       .trigger('change');	
+    $( window ).resize(function() {
+      $(".dupli-slider").each(function() {
+        dupliSlider = $(this);
+        setupSlider(dupliSlider);
+      })
+        .trigger('change');
+    });
     });
   };
+
+  startSliderById(sliderId) {
+    let $ = require('jquery');
+
+    // Initialize all sliders on page
+    let dupliSlider, valuesString, values, length;
+
+    dupliSlider = $("#"+sliderId);
+    let sliderOutput = dupliSlider.next(".slider-output");
+    valuesString = dupliSlider.attr("slider-values");
+    values = valuesString.split('~');
+    var entryValue = dupliSlider.attr("entry-value");
+
+    if (values.length <= 1) {
+    length = values.length - 1;
+      // dupliSlider.prev(".js-duplitrade-slider").addClass("slider-disabled");
+      dupliSlider.closest(".js-duplitrade-slider").addClass("slider-disabled");
+
+    } else {
+      if (entryValue != null) {
+        var entryIndex = values.indexOf(entryValue);
+        dupliSlider.val(entryIndex);
+        dupliSlider.attr("current-value",values[entryIndex]);
+        dupliSlider.attr("current-index",entryIndex);
+      } else {
+        dupliSlider.val(0);
+        dupliSlider.attr("current-value",values[0]);
+        dupliSlider.attr("current-index",0);
+      }
+    }
+    length = values.length - 1;
+    dupliSlider.attr("max",length);
+    dupliSlider.attr("min",0);
+    dupliSlider.attr("step",1);
+    sliderOutput.html("<strong>" + values[0] + "</strong>");
+
+    let newPoint, newPlace, offset;
+    function setupSlider($slider){
+      var width = dupliSlider.width();
+      var valuesString = dupliSlider.attr("slider-values");
+      var values = valuesString.split('~');
+      length = values.length - 1;
+      dupliSlider.attr("max",length);
+      if (values.length > 1){
+        newPoint = (dupliSlider.val() - dupliSlider.attr("min")) / (length - dupliSlider.attr("min"));
+        offset = -6.5;
+        // Prevent bubble from going beyond left or right (unsupported browsers)
+        if (newPoint < 0) {
+          newPlace = 0;
+        } else if (newPoint > 1) {
+          newPlace = width; 
+        } else { 
+          newPlace = width * newPoint + offset; offset -= newPoint;
+        }
+
+        var sliderHighlight = dupliSlider.prev("span");
+        sliderHighlight.width(newPlace+5.8);
+
+        sliderOutput.html('<strong>'+values[dupliSlider.val()]+'</strong>');
+
+        dupliSlider.attr("current-value",values[dupliSlider.val()]);
+        dupliSlider.attr("current-index",dupliSlider.val());
+
+        if(newPoint <= .08){
+          sliderOutput
+            .css({
+              left: 0,
+              right: "unset",
+              marginLeft: 0
+            })
+        } else if(newPoint > .8){
+          sliderOutput
+            .css({
+              right: 0,
+              left: "unset",
+              marginLeft: 0
+            })
+        } else {
+          sliderOutput
+            .css({
+              left: newPlace,
+              right: "unset",
+              marginLeft: offset + "%"
+            })
+        }
+      }
+    }
+
+    // Select all range inputs, watch for change
+    dupliSlider.change(function() {
+      dupliSlider = $(this);
+      setupSlider(dupliSlider);
+    })
+      .trigger('change');	
+
+    dupliSlider.on('input', function() {
+      dupliSlider = $(this);
+      setupSlider(dupliSlider);
+    })
+      .trigger('change');
+
+    $( window ).resize(function() {
+      $(".dupli-slider").each(function() {
+        dupliSlider = $(this);
+        setupSlider(dupliSlider);
+      })
+        .trigger('change');
+    });
+  }
 }
 
 export let sliderManager = new SliderManager();
-export let sliderStyle = require('../css/dupli-slider.css');
+export let sliderStyle = require('../css/blue-dupli-slider.css');
